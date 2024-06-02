e34b444faa090aa6923d16db54eca96e7be881da
techzenithlabs <techzenithlabs@gmail.com>
2024-06-02 14:41:50 +0530

    created register link on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,6 +59,10 @@
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
+            
+        </div>
+        <div class="flex items-center justify-end mt-4">
+           <p class="text-sm text-dark-600">Havent Registered yet, <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">Register</a></p>
         </div>
     </form>
 </x-guest-layout>
