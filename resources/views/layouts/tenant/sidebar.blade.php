e84852b039998490228aea92cea19c878030eabc
techzenithlabs <techzenithlabs@gmail.com>
2024-07-20 16:05:53 +0530

    worked on pricing sectionn

--- resources/views/layouts/tenant/sidebar.blade.php
+++ resources/views/layouts/tenant/sidebar.blade.php
@@ -3,7 +3,7 @@
         <ul>
             <li><a href="{{ route('dashboard') }}"><img class="img-fluid" src="{{ asset('public/assets/images/dashboard.png') }}"> Dashboard</a></li>
             <li><a href="{{ route('tenant.screening') }}/step1"><img class="img-fluid" src="{{ asset('public/assets/images/user.png') }}"> Tenant Screening</a></li>
-            <li><a href="#"><img class="img-fluid" src="{{ asset('public/assets/images/price.png') }}"> Pricing</a></li>
+            <li><a href="{{ route('pricing') }}"><img class="img-fluid" src="{{ asset('public/assets/images/price.png') }}"> Pricing</a></li>
         </ul>
     </div>
 </aside>
